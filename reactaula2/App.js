ade29a4ab1aed21ebcf49ad4b3d62d4744a0034b
marceloluap <58114555+marceloluap@users.noreply.github.com>
2023-05-08 21:08:24 -0300

    Add files via upload

---
+++ reactaula2/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import Login from './components/Login';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ reactaula2/styles/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+});
